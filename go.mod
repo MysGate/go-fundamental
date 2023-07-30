f1b07594bd7bed7531eca51c2f54fa29b9753be7
FleetingTimePaul <83365885@qq.com>
2023-07-30 14:59:18 +0800

    add eth account.

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.19
 
 require (
 	github.com/cskr/pubsub v1.0.2
+	github.com/ethereum/go-ethereum v1.12.0
 	github.com/go-xorm/xorm v0.7.9
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	go.uber.org/zap v1.24.0
@@ -11,10 +12,14 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.2.0 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/holiman/uint256 v1.2.2-0.20230321075855-87b91420868c // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	xorm.io/builder v0.3.6 // indirect
 	xorm.io/core v0.7.2-0.20190928055935-90aeac8d08eb // indirect
 )
---
+++ util/eth_account.go
@@ -0,0 +1,51 @@
+package util
+
+import (
+	"crypto/ecdsa"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+)
+
+type EthAccount struct {
+	privateKey string
+	address    string
+	publicKey  string
+}
+
+func (ea *EthAccount) ToString() string {
+	return fmt.Sprintf("%s:%s", ea.address, ea.privateKey)
+}
+
+func NewEthAccount() *EthAccount {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		errMsg := fmt.Sprintf("NewEthAccount err:%+v", err)
+		Logger().Error(errMsg)
+		return nil
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println(hexutil.Encode(privateKeyBytes)[2:])
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		Logger().Error("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+		return nil
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Println(hexutil.Encode(publicKeyBytes)[4:])
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println(address)
+
+	ea := &EthAccount{
+		address:    address,
+		publicKey:  hexutil.Encode(publicKeyBytes)[4:],
+		privateKey: hexutil.Encode(privateKeyBytes)[2:],
+	}
+	return ea
+}
